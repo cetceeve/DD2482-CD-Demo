9e539fd4afbc5f64d4edda5a1bbf1fbdff0edc91
sifisKoen <sifis.k.i@gmail.com>
2023-03-28 01:53:06 +0200

    Server initial implementation with Client

---
+++ src/staticServer.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/get_url', methods=['GET'])
+def get_url():
+    google_url = "https://www.google.com"
+    return jsonify({"url": google_url})
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
